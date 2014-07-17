affd350ec623b4a0a6be0e4f055017f89671dc7e
Jun Kimura <jksmphone@gmail.com>
2014-07-18 02:19:50 +0900

    Update to version 0.2.2

--- flask_request_params/__init__.py
+++ flask_request_params/__init__.py
@@ -1,5 +1,5 @@
 #-*- coding:utf-8 -*-
 from .params import get_request_params, bind_request_params, Params
 
-__version_info__ = ('0', '2', '2', 'dev')
+__version_info__ = ('0', '2', '2')
 __version__ = '.'.join(__version_info__)
