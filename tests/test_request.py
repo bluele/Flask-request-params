dece208508a32f62359f56b1eef30d8d4a1df1ae
Jun Kimura <jksmphone@gmail.com>
2014-07-24 00:03:03 +0900

    Added test case for post json parameters.

--- tests/test_request.py
+++ tests/test_request.py
@@ -16,3 +16,13 @@ class TestRequestCase(TestCase):
         data = json.loads(response.data.decode('utf8'))
         assert 'one' in data and 'two' in data
         assert data['one'] == '1' and data['two'] == '2'
+
+    def test_json_parameters(self):
+        response = self.client.post(
+            '/',
+            headers=[('Content-Type', 'application/json')],
+            data=json.dumps({'one': '1', 'two': '2'})
+        )
+        data = json.loads(response.data.decode('utf8'))
+        assert 'one' in data and 'two' in data
+        assert data['one'] == '1' and data['two'] == '2'
